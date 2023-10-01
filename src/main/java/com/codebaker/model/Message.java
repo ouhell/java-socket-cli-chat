9a18bed8701a674a00d4d2ef449acfd1dc65113d
ouhell <oussamabarca200@gmail.com>
2023-10-01 19:21:16 +0100

    add no args constructor to Message

--- src/main/java/com/codebaker/model/Message.java
+++ src/main/java/com/codebaker/model/Message.java
@@ -2,9 +2,11 @@ package com.codebaker.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Message {
     private String content;
     private String sender;
